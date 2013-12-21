2ed25890283c100f87f8da48af2918d683aca764
raphaelchang <raphaelchang@f74aeb85-c29b-4e46-8eb1-f4d822aa94e9>
2013-12-21 05:25:28 +0000

    Importing new LRT14 project

---
+++ LRT14/LRTRobotBase.cpp
@@ -0,0 +1,45 @@
+#include "LRTRobotBase.h"
+#include <sysLib.h>
+
+LRTRobotBase::LRTRobotBase()
+: m_loopSynchronizer((TimerEventHandler) LRTRobotBase::ReleaseLoop, this)
+{
+	m_loopSynchSem = semBCreate(SEM_Q_PRIORITY, SEM_FULL);
+	semTake(m_loopSynchSem, WAIT_FOREVER);
+}
+
+LRTRobotBase::~LRTRobotBase()
+{
+	printf("robot destructing\n");
+}
+
+void LRTRobotBase::StartCompetition()
+{
+	printf("vxWorks task: %s\n", m_task->GetName()); //Maybe this line was crashing it...
+
+	GetWatchdog().SetEnabled(false);
+	m_loopSynchronizer.StartPeriodic(1.0/50.0);
+
+	RobotInit();
+	Run();
+}
+
+void LRTRobotBase::Run()
+{
+	while(true)
+	{
+		semTake(m_loopSynchSem, WAIT_FOREVER);
+		Main();
+	}
+}
+
+void LRTRobotBase::Main()
+{
+	printf("LRTRobotBase::Run(): Override me!");
+}
+
+void LRTRobotBase::ReleaseLoop(void *param)
+{
+	LRTRobotBase *robot = (LRTRobotBase *) param;
+	semGive(robot->m_loopSynchSem);
+}
---
+++ LRT14/LRTRobotBase.h
@@ -0,0 +1,33 @@
+#ifndef LRT_ROBOT_BASE_
+#define LRT_ROBOT_BASE_
+
+#include <WPILib.h>
+#include <semLib.h>
+
+class LRTRobotBase : public RobotBase
+{
+public:
+	LRTRobotBase();
+	virtual ~LRTRobotBase();
+	
+	virtual void RobotInit() = 0;
+	
+	// Inherited from RobotBase, starts the competition
+	virtual void StartCompetition();
+	
+	// Function to execute each pass in the loop; similar to a main function, but it gets run multiple times.
+	virtual void Main();
+
+private:
+	void Run();
+
+	Notifier m_loopSynchronizer;
+	static bool m_isRunning;
+
+	static void ReleaseLoop(void * param);
+	
+	SEM_ID m_loopSynchSem;
+	bool m_robotMainOverridden;
+};
+
+#endif
