6b713cfb2bb4c4a86f4356813262086a666e12e8
tonypeng1337 <tonypeng1337@f74aeb85-c29b-4e46-8eb1-f4d822aa94e9>
2014-01-09 09:20:35 +0000

    Add RUnit to LRT14

---
+++ LRT14/UnitTests/SampleUnitTest.cpp
@@ -0,0 +1,22 @@
+INT32 TestDummy()
+{
+	RU_ASSERT(true, "The universe is broken.");
+	
+	return 0;
+}
+
+extern "C"
+{
+	INT32 FRC_UserProgram_StartupLibraryInit()
+	{
+		TestApparatus app;
+		
+		app.EnableOption(TestApparatus::OPT_BASIC);
+		
+		app.RegisterTest((RU_FUNCPTR)TestDummy, "Dummy Task");
+		
+		app.Run();
+		
+		return 0;
+	}
+}
