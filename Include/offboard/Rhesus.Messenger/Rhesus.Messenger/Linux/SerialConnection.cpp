9830396ca2e570bd27aa8195dd7e2f05c49bb18d
tonypeng1337 <tonypeng1337@f74aeb85-c29b-4e46-8eb1-f4d822aa94e9>
2014-01-14 07:11:08 +0000

    Add linux implementation for sending -- TODO: reading

--- Include/offboard/Rhesus.Messenger/Rhesus.Messenger/Linux/SerialConnection.cpp
+++ Include/offboard/Rhesus.Messenger/Rhesus.Messenger/Linux/SerialConnection.cpp
@@ -30,7 +30,45 @@ void SerialConnection::Open()
 		return;
 	}
 
+	cfsetospeed (&ts, kBaudRateCRio9074);
+	cfsetispeed (&ts, kBaudRateCRio9074);
 
+	ts.c_cflag = (ts.c_cflag & ~CSIZE) | CS8;
+    ts.c_iflag &= ~IGNBRK;
+    ts.c_lflag = 0;
+    ts.c_oflag = 0;
+    ts.c_cc[VMIN]  = 0;
+    ts.c_cc[VTIME] = 10;
+    ts.c_iflag &= ~(IXON | IXOFF | IXANY);
+    ts.c_cflag |= (CLOCAL | CREAD);
+    ts.c_cflag &= ~(PARENB | PARODD);
+    ts.c_cflag |= parity;
+    ts.c_cflag &= ~CSTOPB;
+    ts.c_cflag &= ~CRTSCTS;
+
+	if(tcsetattr(pFD, TCSANOW, &ts) != 0)
+	{
+		// RHESUS NO THROW
+		return;
+	}
+}
+
+INT32 SerialConnection::SendRaw(MessageBuffer& buff)
+{
+	const UINT8* buff = buff.GetBuffer();
+	int len = buff.GetBufferLength();
+
+	if(len < 0 || len > 255)
+	{
+		// RHESUS NO THROW
+		return -1;
+	}
+
+	UINT8 cLen = (UINT8)len;
+
+	write(pFD, &cLen, 1);
+	write(pFD, buff, len);
+	fsync(pFD);
 }
 
 #endif
