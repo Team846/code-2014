6bcc37be8eb1fed13b0a0ec221b64822c2c674b1
tonypeng1337 <tonypeng1337@f74aeb85-c29b-4e46-8eb1-f4d822aa94e9>
2014-01-14 06:59:38 +0000

    [Rhesus] Fix some minor bugs/typos in do_async.

--- LRT14/Rhesus/Toolkit/Tasks/do_async.hpp
+++ LRT14/Rhesus/Toolkit/Tasks/do_async.hpp
@@ -33,7 +33,7 @@ namespace Tasks
 		};
 		
 	public:
-		static INT32 AsyncMethodWrapper(TaskStruct t) {
+		static INT32 AsyncMethodWrapper(TaskStruct* t) {
 			//		printf("!!in wrapper!!\n");
 	
 			//			do_async* instance = reinterpret_cast<do_async*>(p0);
@@ -43,11 +43,13 @@ namespace Tasks
 			//		printf("calling func: %u\n", p0);
 	
 			//call function
-			INT32 retCode = (t.ptr->*t.mptr)(t.p0, t.p1, t.p2, t.p3, t.p4, t.p5, t.p6, t.p7);
+			INT32 retCode = (t->ptr->*t->mptr)(t->p0, t->p1, t->p2, t->p3, t->p4, t->p5, t->p6, t->p7);
 	
 			//callback
-			if (t.ccptr != NULL &&t.cptr != NULL)
-				(t.ccptr->*t.cptr)(retCode);
+			if (t->ccptr != NULL &&t->cptr != NULL)
+				(t->ccptr->*t->cptr)(retCode);
+			
+			DELETE(t);
 			
 			return 0;
 		}
@@ -57,9 +59,9 @@ namespace Tasks
 				UINT32 p4 = 0, UINT32 p5 = 0, UINT32 p6 = 0, UINT32 p7 = 0)
 		{
 			//		printf("!!starting wrapper!!\n");
-			TaskStruct t = create(inst, asyncFunc, NULL, NULL, p0, p1, p2,p3, p4, p5, p6, p7);
-			TaskPool::EnqueueTask((FUNCPTR) AsyncMethodWrapper(t));
-			//			m_asyncTask.Start((UINT32)this, p0, p1, p2, p3, p4, p5, p6, p7);
+			TaskStruct* t = create(inst, asyncFunc, NULL, NULL, p0, p1, p2,p3, p4, p5, p6, p7);
+			TaskPool::EnqueueTask((FUNCPTR) AsyncMethodWrapper, (UINT32)t);
+			//			m_asyncTask->Start((UINT32)this, p0, p1, p2, p3, p4, p5, p6, p7);
 		}
 	
 		do_async(AsyncMethodClassPtr inst, AsyncMethodFuncPtr asyncFunc,
@@ -68,30 +70,30 @@ namespace Tasks
 				UINT32 p4 = 0, UINT32 p5 = 0, UINT32 p6 = 0, UINT32 p7 = 0)
 		{
 			//		printf("!!starting wrapper!!\n");
-			TaskStruct t = create(inst, asyncFunc, callback, c_inst, p0, p1, p2,p3, p4, p5, p6, p7);
-			TaskPool::EnqueueTask((FUNCPTR) AsyncMethodWrapper(t));
-			//			m_asyncTask.Start((UINT32)this, p0, p1, p2, p3, p4, p5, p6, p7);
+			TaskStruct* t = create(inst, asyncFunc, callback, c_inst, p0, p1, p2,p3, p4, p5, p6, p7);
+			TaskPool::EnqueueTask((FUNCPTR) AsyncMethodWrapper, (UINT32)t);
+			//			m_asyncTask->Start((UINT32)this, p0, p1, p2, p3, p4, p5, p6, p7);
 		}
 	
 	private:
 	
-		TaskStruct create(AsyncMethodClassPtr ptr, AsyncMethodFuncPtr mptr,
+		TaskStruct* create(AsyncMethodClassPtr ptr, AsyncMethodFuncPtr mptr,
 				AsyncCallbackFuncPtr cptr, AsyncCallbackClassPtr ccptr, UINT32 arg0, UINT32 arg1, UINT32 arg2,
 				UINT32 arg3, UINT32 arg4, UINT32 arg5, UINT32 arg6, UINT32 arg7) 
 		{
-			TaskStruct t;
-			t.ptr = ptr;
-			t.mptr = mptr;
-			t.cptr = cptr;
-			t.ccptr = ccptr;
-			t.arg0 = arg0;
-			t.arg1 = arg1;
-			t.arg2 = arg2;
-			t.arg3 = arg3;
-			t.arg4 = arg4;
-			t.arg5 = arg5;
-			t.arg6 = arg6;
-			t.arg7 = arg7;
+			TaskStruct* t = new TaskStruct();
+			t->ptr = ptr;
+			t->mptr = mptr;
+			t->cptr = cptr;
+			t->ccptr = ccptr;
+			t->arg0 = arg0;
+			t->arg1 = arg1;
+			t->arg2 = arg2;
+			t->arg3 = arg3;
+			t->arg4 = arg4;
+			t->arg5 = arg5;
+			t->arg6 = arg6;
+			t->arg7 = arg7;
 			return t;
 		}
 	
