00622c0fc672b3280ff83bc0e31ae45849e560d5
FrankLin <FrankLin@f74aeb85-c29b-4e46-8eb1-f4d822aa94e9>
2014-01-26 22:37:20 +0000

    Created launcher angle data

--- LRT14/ComponentData/LauncherAngleData.cpp
+++ LRT14/ComponentData/LauncherAngleData.cpp
@@ -3,7 +3,7 @@
 LauncherAngleData::LauncherAngleData() :
 	ComponentData("LauncherAngleData")
 {
-	
+	m_launcherAngle = LOW;
 }
 
 LauncherAngleData* LauncherAngleData::Get()
@@ -19,4 +19,15 @@ void LauncherAngleData::ResetCommands()
 void LauncherAngleData::Log()
 {
 	
+
+}
+
+LauncherAngleData::Angle LauncherAngleData::GetAngle()
+{
+	return m_launcherAngle;
+}
+
+void LauncherAngleData::SetAngle(LauncherAngleData::Angle angle)
+{
+	m_launcherAngle = angle;
 }
--- LRT14/ComponentData/LauncherAngleData.h
+++ LRT14/ComponentData/LauncherAngleData.h
@@ -6,11 +6,23 @@
 class LauncherAngleData : public ComponentData
 {
 public:
+	enum Angle
+	{
+		LOW,
+		HIGH
+	};
+	
 	LauncherAngleData();
 	static LauncherAngleData* Get();
+	
+	Angle GetAngle();
+	void SetAngle(Angle angle);
 
 	void ResetCommands();
 	void Log();
+	
+private:
+	Angle m_launcherAngle;
 };
 
 #endif
