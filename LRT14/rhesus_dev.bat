c753489fcf8ef401c16f4d5a71ca5c36a9c733a0
tonypeng1337 <tonypeng1337@f74aeb85-c29b-4e46-8eb1-f4d822aa94e9>
2014-01-26 05:36:36 +0000

    Update dev setup script to not delete the entire folder but only the subfolders.

--- LRT14/rhesus_dev.bat
+++ LRT14/rhesus_dev.bat
@@ -1,4 +1,5 @@
-if exist Rhesus del Rhesus
-mkdir Rhesus
+if not exist Rhesus mkdir Rhesus
+if exist "Rhesus/Toolkit" del "Rhesus/Toolkit" /y
+if exist "Rhesus/Messenger" del "Rhesus/Messenger" /y
 mklink /J "Rhesus/Toolkit" "../Include/PPC603gnu/Rhesus/Toolkit"
 mklink /J "Rhesus/Messenger" "../Include/common/Rhesus/Messenger"
