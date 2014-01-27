5b043e59d63d7c2e1168a1e0a1ab5ece445ace65
tonypeng1337 <tonypeng1337@f74aeb85-c29b-4e46-8eb1-f4d822aa94e9>
2014-01-27 01:18:15 +0000

    Implement FRCDashboard

---
+++ LRT14/Communication/FRCDashboard.cpp
@@ -0,0 +1,94 @@
+#include "FRCDashboard.h"
+
+using namespace Rhesus::Toolkit::Tasks;
+using namespace Rhesus::Messenger;
+
+FRCDashboard* FRCDashboard::m_instance = NULL;
+
+FRCDashboard* FRCDashboard::instance()
+{
+	if(m_instance == NULL)
+	{
+		m_instance = new FRCDashboard();
+	}
+	
+	return m_instance;
+}
+
+void FRCDashboard::Close()
+{
+	if(m_instance == NULL)
+	{
+		return;
+	}
+	
+	DELETE(m_instance);
+}
+
+FRCDashboard::FRCDashboard()
+{
+	m_queueMutex = Mutex();
+	
+	m_server = new NetServer(1140);
+	m_server->Open();
+}
+
+FRCDashboard::~FRCDashboard()
+{
+	m_server->Close();
+	DELETE(m_server);
+}
+
+void FRCDashboard::Tick()
+{
+	instance()->Tick;
+}
+
+void FRCDashboard::Flush()
+{
+	instance()->Flush();
+}
+
+void FRCDashboard::EnqueueMessage(NetBuffer& buff, NetChannel::Enum method, int channel)
+{
+	instance()->EnqueueMessage(buff, method, channel);
+}
+
+void FRCDashboard::tick()
+{
+	// perhaps later, we may want to send some heartbeats or packets of the sort
+	
+	// Flush the buffer
+	Flush();
+}
+
+void FRCDashboard::flush()
+{
+	{
+		lock_on l(m_queueMutex);
+		
+		for(unsigned int i = 0; i < m_netBufferQueue.size(); i++)
+		{
+			DMessage nb = m_netBufferQueue.front();
+			m_netBufferQueue.pop();
+			
+			m_server->SendToAll(nb.nb, nb.method, nb.channel);
+			
+			DELETE(nb.nb);
+		}
+	} // m_queueSem
+}
+
+void FRCDashboard::enqueueMessage(NetBuffer& buff, NetChannel::Enum method, int channel)
+{
+	DMessage msg;
+	msg.nb = new NetBuffer(buff.GetBuffer(), buff.GetBufferLength());
+	msg.method = method;
+	msg.channel = channel;
+			
+	{
+		lock_on l(m_queueMutex);
+	
+		m_netBufferQueue.push(msg);
+	}
+}
--- LRT14/Communication/FRCDashboard.h
+++ LRT14/Communication/FRCDashboard.h
@@ -1,6 +1,64 @@
 #ifndef FRCDASH_DASHBOARD_H_
 #define FRCDASH_DASHBOARD_H_
 
+#include <WPILib.h>
+#include <semLib.h>
 
+#include <queue>
+#include <sstream>
+
+#include <Rhesus/Toolkit/Tasks/Rhesus.Toolkit.Tasks.h>
+
+#include <Rhesus/Messenger/NetServer.h>
+#include <Rhesus/Messenger/NetBuffer.h>
+#include <Rhesus/Messenger/NetChannel.h>
+
+class FRCDashboard
+{
+private:
+	typedef struct
+	{
+		Rhesus::Messenger::NetBuffer* nb;
+		Rhesus::Messenger::NetChannel::Enum method;
+		int channel;
+	} DMessage;
+	
+public:
+	static void Close();
+	
+	/*!
+	 * Updates the dashboard and flushes the message queue.
+	 */
+	static void Tick();
+	
+	/*!
+	 * @brief Forces a flush of the message queue.
+	 */
+	static void Flush();
+
+	static void EnqueueMessage(Rhesus::Messenger::NetBuffer& buff, 
+			Rhesus::Messenger::NetChannel::Enum method, int channel);
+	
+private:
+	
+	static FRCDashboard* instance();
+	
+	static FRCDashboard* m_instance;
+	
+	FRCDashboard();
+	~FRCDashboard();
+	
+	void tick();
+	
+	void flush();
+
+	void enqueueMessage(Rhesus::Messenger::NetBuffer& buff, 
+			Rhesus::Messenger::NetChannel::Enum method, int channel);
+	
+	Rhesus::Toolkit::Tasks::Mutex m_queueMutex;
+	std::queue<DMessage> m_netBufferQueue;
+	
+	Rhesus::Messenger::NetServer* m_server;
+};
 
 #endif
