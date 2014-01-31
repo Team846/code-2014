6bc916a37f03ea90a2e9931db21aa50396c43001
erichuynh <erichuynh@f74aeb85-c29b-4e46-8eb1-f4d822aa94e9>
2014-01-31 03:18:02 +0000

    Wrote CollectorArm class.

--- LRT14/Components/CollectorArm.cpp
+++ LRT14/Components/CollectorArm.cpp
@@ -8,18 +8,37 @@ CollectorArm::CollectorArm() :
 	Component("CollectorArm", DriverStationConfig::DigitalIns::COLLECTOR_ARM),
 	Configurable("CollectorArm")
 {
+	m_armData = CollectorArmData::Get();
 	m_talon = new LRTTalon(ConfigPortMappings::Get("PWM/COLLECTOR_ARM"), "CollectorArm");
-	analogChannel = new AnalogChannel(ConfigPortMappings::Get("Analog/COLLECTOR_ARM"));
+	m_pot = new AnalogChannel(ConfigPortMappings::Get("Analog/COLLECTOR_ARM"));
 }
 
 CollectorArm::~CollectorArm()
 {
-	
+	DELETE(m_talon);
+	DELETE(m_pot);
 }
 
 void CollectorArm::OnEnabled()
 {
-	
+	switch (m_armData->GetPosition())
+	{
+	case CollectorArmData::STOWED:
+		m_talon->SetDutyCycle(ComputeOutput(m_stowedSetpoint));
+		break;
+	case CollectorArmData::COLLECT:
+		m_talon->SetDutyCycle(ComputeOutput(m_collectSetpoint));
+		break;
+	default:
+		m_talon->SetDutyCycle(0.0);
+	}
+}
+
+float CollectorArm::ComputeOutput(int setpoint)
+{
+	float error = (float)(setpoint - m_pot->GetAverageValue()) / (float)m_maxPotValue;
+	int sign = (error > 0 ? 1 : -1);
+	return sign * pow(fabs(error), 1.0/3.0);
 }
 
 void CollectorArm::OnDisabled()
@@ -41,4 +60,5 @@ void CollectorArm::Configure()
 {
 	m_collectSetpoint = GetConfig("collect_setpoint", 0);
 	m_stowedSetpoint = GetConfig("stowed_setpoint", 0);
+	m_maxPotValue = GetConfig("max_pot_value", 0);
 }
