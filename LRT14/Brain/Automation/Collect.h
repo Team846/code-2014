83b26c62782724b1b2e2d299901871772a6b0b42
FrankLin <FrankLin@f74aeb85-c29b-4e46-8eb1-f4d822aa94e9>
2014-02-01 05:37:03 +0000

    Stop ball in bumper and purge if wrong ball color

--- LRT14/Brain/Automation/Collect.h
+++ LRT14/Brain/Automation/Collect.h
@@ -5,6 +5,7 @@
 #include "../../ComponentData/CollectorArmData.h"
 #include "../../ComponentData/CollectorRollersData.h"
 #include "../../Config/Configurable.h"
+#include "../../Sensors/SensorFactory.h"
 
 class Collect : public Automation, public Configurable
 {
@@ -20,7 +21,19 @@ public:
 	
 private:
 	CollectorArmData *m_collectorArm;
-	CollectorRollersData *m_collectorRollers;
+	CollectorRollersData *m_collectorRollers; 
+	GearTooth *m_gearTooth;
+	float m_ballCollectionThreshold;
+	AnalogChannel* m_redChannel;
+	AnalogChannel* m_greenChannel;
+	AnalogChannel* m_blueChannel;
+	int m_redBallRed;
+	int m_redBallBlue;
+	int m_redBallGreen;
+	int m_blueBallRed;
+	int m_blueBallBlue;
+	int m_blueBallGreen;
+	int m_colorRange;
 };
 
 #endif
