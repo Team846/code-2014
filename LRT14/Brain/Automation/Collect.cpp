61d57203ff92428887dd44edf0445def7b0dbbb4
FrankLin <FrankLin@f74aeb85-c29b-4e46-8eb1-f4d822aa94e9>
2014-02-01 05:37:03 +0000

    Stop ball in bumper and purge if wrong ball color

--- LRT14/Brain/Automation/Collect.cpp
+++ LRT14/Brain/Automation/Collect.cpp
@@ -1,4 +1,6 @@
 #include "Collect.h"
+#include "../../Config/ConfigPortMappings.h"
+#include "../../RobotState.h"
 
 Collect::Collect() :
 	Automation("Collect", true),
@@ -6,6 +8,10 @@ Collect::Collect() :
 {
 	m_collectorArm = CollectorArmData::Get();
 	m_collectorRollers = CollectorRollersData::Get();
+	m_gearTooth = SensorFactory::GetGearTooth(ConfigPortMappings::Get("Digital/COLLECTOR_GEAR_TOOTH"));
+	m_redChannel = SensorFactory::GetAnalogChannel(ConfigPortMappings::Get("Analog/COLOR_RED"));
+	m_greenChannel = SensorFactory::GetAnalogChannel(ConfigPortMappings::Get("Analog/COLOR_GREEN"));
+	m_blueChannel = SensorFactory::GetAnalogChannel(ConfigPortMappings::Get("Analog/COLOR_BLUE"));
 }
 
 void Collect::AllocateResources()
@@ -27,6 +33,39 @@ bool Collect::Run()
 		m_collectorRollers->SetRunning(true);
 		m_collectorRollers->SetDirection(CollectorRollersData::FORWARD);
 		m_collectorRollers->SetSpeed(1.0);
+		bool wrongBall = false;
+		if (RobotState::Instance().Alliance() == DriverStation::kRed)
+		{
+			if (m_redChannel->GetAverageValue() <= m_blueBallRed + m_colorRange
+					&& m_redChannel->GetAverageValue() >= m_blueBallRed - m_colorRange
+					&& m_blueChannel->GetAverageValue() <= m_blueBallBlue + m_colorRange
+					&& m_blueChannel->GetAverageValue() >= m_blueBallBlue - m_colorRange
+					&& m_greenChannel->GetAverageValue() <= m_blueBallGreen + m_colorRange
+					&& m_greenChannel->GetAverageValue() >= m_blueBallGreen - m_colorRange)
+			{
+				m_collectorRollers->SetDirection(CollectorRollersData::REVERSE);
+				wrongBall = true;
+			}
+		}
+		if (RobotState::Instance().Alliance() == DriverStation::kBlue)
+		{
+			if (m_redChannel->GetAverageValue() <= m_redBallRed + m_colorRange
+					&& m_redChannel->GetAverageValue() >= m_redBallRed - m_colorRange
+					&& m_blueChannel->GetAverageValue() <= m_redBallBlue + m_colorRange
+					&& m_blueChannel->GetAverageValue() >= m_redBallBlue - m_colorRange
+					&& m_greenChannel->GetAverageValue() <= m_redBallGreen + m_colorRange
+					&& m_greenChannel->GetAverageValue() >= m_redBallGreen - m_colorRange)
+			{
+				m_collectorRollers->SetDirection(CollectorRollersData::REVERSE);
+				wrongBall = true;
+			}
+		}
+		
+		if (1 / m_gearTooth->GetPeriod() < m_ballCollectionThreshold && !wrongBall)
+		{
+			m_collectorRollers->SetRunning(false);
+			return true;
+		}
 		return false;
 	}
 	else
@@ -44,5 +83,12 @@ bool Collect::Abort()
 
 void Collect::Configure()
 {
-
+	m_ballCollectionThreshold = GetConfig("ball_collection_threshold", 0);
+	m_redBallRed = GetConfig("red_ball_red", 0);
+	m_redBallBlue = GetConfig("red_ball_blue", 0);
+	m_redBallGreen = GetConfig("red_ball_green", 0);
+	m_blueBallRed = GetConfig("blue_ball_red", 0);
+	m_blueBallBlue = GetConfig("blue_ball_blue", 0);
+	m_blueBallGreen = GetConfig("blue_ball_green", 0);
+	m_colorRange = GetConfig("color_range", 5);
 }
