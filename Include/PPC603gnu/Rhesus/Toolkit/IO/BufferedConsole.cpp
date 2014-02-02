43b21811ef5fbe16c18ee3bbb19b9e6cc14f3cce
tonypeng1337 <tonypeng1337@f74aeb85-c29b-4e46-8eb1-f4d822aa94e9>
2014-02-02 09:19:51 +0000

    Fix bad ref bug introduced by 109 (move passed argument to the heap to persist after method returns)

--- Include/PPC603gnu/Rhesus/Toolkit/IO/BufferedConsole.cpp
+++ Include/PPC603gnu/Rhesus/Toolkit/IO/BufferedConsole.cpp
@@ -29,11 +29,11 @@ void BufferedConsole::Printfln(std::string msg, ...)
 	vsprintf(buffer, msg.c_str(), args);
 	va_end(args);
 	
-	string n(buffer);
+	string* n = new string(buffer);
 	
 //	std::printf("ENQUED...");
 	
-	TaskPool::EnqueueTask((FUNCPTR)InternalPrintWrapper, (UINT32)&n);
+	TaskPool::EnqueueTask((FUNCPTR)InternalPrintWrapper, (UINT32)n);
 }
 
 void BufferedConsole::Printf(std::string msg, ...)
