8e8b588b4cde2b59e03aa9aff7eff86fa5386f9c
tonypeng1337 <tonypeng1337@f74aeb85-c29b-4e46-8eb1-f4d822aa94e9>
2014-02-10 04:53:41 +0000

    Fix DELETE/DELETE_ARR macro issue identified in issue #220 (closes #220).  Move CountingSemaphore SEM_ID constructor code inside a vxworks conditional.

---
+++ LRT14/UnitTests/RUnit/RUnit_Utils.h
@@ -0,0 +1,13 @@
+//#ifndef RUNIT_UTILS_H_
+//#define RUNIT_UTILS_H_
+//
+//#include "RUnitException.h"
+//
+//#include <string>
+//
+//inline void RU_log(const char* format, ...)
+//{
+//	
+//}
+//
+//#endif
