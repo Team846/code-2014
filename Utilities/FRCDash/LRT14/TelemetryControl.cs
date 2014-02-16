9c03614311363dff4f4c0668d477539bde972863
AbhiramKothapalli <AbhiramKothapalli@f74aeb85-c29b-4e46-8eb1-f4d822aa94e9>
2014-02-16 02:15:51 +0000

    Committing TelemetryControl.cs, display now works

---
+++ Utilities/FRCDash/LRT14/TelemetryControl.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+using TomShane.Neoforce.Controls;
+using STDConsole = System.Console;
+
+using RhesusNet.NET;
+
+using Dashboard.Library;
+using Dashboard.Library.Persistence;
+
+namespace LRT14
+{
+    class TelemetryControl : DashboardControl
+    {
+        private int topPadding;
+        private int leftPadding;
+        private int topMargin;
+        private int sideMargin;
+        private int textBoxHeight;
+        private int textBoxWidth;
+        private int labelInfoDistance;
+        private String[] labels;
+
+        private string id;
+        private string persistenceKey;
+        private ContentLibrary content;
+      
+        public TelemetryControl(Manager manager, string id, string persistenceKey, ContentLibrary content)
+            : base(manager, id, persistenceKey, content)
+        {
+            this.id = id;
+            this.persistenceKey = persistenceKey;
+            this.content = content;
+
+            Color = Color.Transparent;
+        }
+
+        public void display()
+        {
+
+
+            for (int i = 0; i < labels.Length; i++)
+            {
+                Label label = new Label(Manager);
+                label.Text = labels[i];
+                label.Left = leftPadding;
+                label.Top = topPadding + topMargin * i + textBoxHeight * i;
+                label.Parent = this;
+                label.Init();
+
+
+                TextBox info = new TextBox(Manager);
+                info.Text = "data";
+                info.Left = label.Left + 30;
+                info.Height = label.Height;
+
+                
+            }
+            
+            
+        }
+
+        public int topPaddingMod
+        {
+            get { return topPadding; }
+            set { topPadding = value; }
+        }
+
+        public int leftPaddingMod
+        {
+            get { return leftPadding;}
+            set { leftPadding = value;}
+        }
+
+        public int topMarginMod
+        {
+            get { return topMargin; }
+            set { topMargin = value; }
+        }
+
+        public int sideMarginMod
+        {
+            get { return sideMargin; }
+            set { sideMargin = value; }
+        }
+
+        public int textBoxHeightMod
+        {
+            get { return textBoxHeight; }
+            set { textBoxHeight = value; }
+        }
+
+        public int textBoxWidthMod
+        {
+            get { return textBoxHeight; }
+            set { textBoxHeight = value; }
+        }
+
+        public String[] labelsMod
+        {
+            get { return labels; }
+            set{labels = value;}
+        }
+
+        public int labelInfoDistanceMod
+        {
+            get{return labelInfoDistance;}
+            set{labelInfoDistance = value;}
+        }
+
+
+
+    }
+
+
+    class teleData
+    {
+
+        public string getData(string id)
+        {
+
+
+            return null;
+
+        }
+
+        
+    }
+
+}
