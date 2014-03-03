0311a50bb5d31c948d5d437bac45067cd1bb9b41
PatrickDong <PatrickDong@f74aeb85-c29b-4e46-8eb1-f4d822aa94e9>
2014-03-03 21:31:01 +0000

    Update with fixes to parsing teams into matrix

--- Utilities/Scouting App/Matrix.java
+++ Utilities/Scouting App/Matrix.java
@@ -1,29 +1,78 @@
 import com.tonypeng.api.thebluealliance.BLUE;
 import com.tonypeng.api.thebluealliance.BLUE.BLUEApiException;
+import com.tonypeng.api.thebluealliance.BLUE.Teams.Team;
+
 
 public class Matrix
 {
     final int NUM_MATCHES = 99;
-    final int NUM_TEAMS = 59;
-    double[][] matches = new double[2 * NUM_MATCHES][NUM_TEAMS];
-    double[][] play_matches = new double[NUM_TEAMS][NUM_TEAMS];
-    
-    public static void main( String[] args )
+
+    final static int NUM_TEAMS = 59;
+
+    static int[] list_teams = new int[NUM_TEAMS];
+
+    int[][] matches = new int[2 * NUM_MATCHES][NUM_TEAMS];
+
+    int[][] play_matches = new int[NUM_TEAMS][NUM_TEAMS];
+
+
+    public static void main( String[] args ) throws BLUEApiException
     {
         BLUE.setAppId( "frc846:scouting_app:v01" );
-        double[][] matrix = { { 1, 2, 2 }, { 1, 1, 3 } };
-        RREF( matrix );
-        printMatrix( matrix );
+        setArrays();
+        //double[][] matrix = { { 1, 2, 2 }, { 1, 1, 3 } };
+        //RREF( matrix );
+        //printMatrix( matrix );
     }
+
 
     public static void setArrays() throws BLUEApiException
     {
+        int error = 0;
+        int count2 = 0;
         BLUE.Events.Event event = BLUE.Events.getEvent( "casj", 2013 );
         BLUE.Teams.Team[] teams = event.getTeams();
-        BLUE.Matches.Match[] matches = event.getMatches();
+        int[] list_teams = new int[NUM_TEAMS];
+        for(int j = 0; j < teams.length; j++)
+        {
+            String[] teams_split = teams[j].toString().split( "}" );
+            BLUE.Matches.Match[] matches = event.getMatches();
+            for ( int i = 0; i < teams_split.length; i++ )
+            {
+                for(int k = 0; k < teams_split[i].length() - 2; k++)
+                {
+                    if(teams_split[i].substring( k, k+3 ).equals( "USA" ))
+                    {
+                        //System.out.println(teams_split[i].substring( k+5 ));
+                        int end_num = teams_split[i].indexOf( ",", k+5 );
+                        String team_number = teams_split[i].substring(  k+5, end_num );
+                        for(int count = 0; count < team_number.length(); count++)
+                        {
+                            if(!Character.isDigit(team_number.charAt( 0 )))
+                            {
+                                error = 1;
+                            }
+                        }
+                        if(error == 0)
+                        {
+                            //System.out.println(Integer.parseInt( team_number ));
+                            list_teams[count2] = Integer.parseInt( team_number );
+                            count2++;
+                        }
+                        error = 0;
+                    }
+                }
+
+            }
+        }
+        for(int i = 0; i < list_teams.length; i++)
+        {
+            System.out.println(list_teams[i]);
+            System.out.println(i);
+        }
     }
-    
-    
+
+
     public static void printMatrix( double[][] matrix )
     {
         int n = matrix.length;
