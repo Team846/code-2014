2e642fc70d81ab075a91c80e75ca55d2d3db8bbe
manojvasishta <manojvasishta@f74aeb85-c29b-4e46-8eb1-f4d822aa94e9>
2014-03-14 03:28:45 +0000

    cRIO-side LED code untested

---
+++ LRT14/Components/Indicator.cpp
@@ -0,0 +1,36 @@
+#include "Indicator.h"
+#include "../Config/DriverStationConfig.h"
+
+Indicator::Indicator():
+	Component("Indicator",DriverStationConfig::DigitalIns::NO_DS_DI)
+{
+	DigitalModule* module = DigitalModule::GetInstance(4);
+	m_i2c = module->GetI2C(168);
+	m_i2c->SetCompatibilityMode(true);
+	m_indicatorData = IndicatorData::Get();
+}
+
+void Indicator::OnEnabled()
+{
+}
+
+void Indicator::OnDisabled()
+{
+}
+
+void Indicator::UpdateEnabled()
+{
+	char shooterPattern = (char)m_indicatorData->GetShooterPattern();
+	char distancePattern = (char)m_indicatorData->GetDistancePattern();
+	char combinedPattern = (char)(((unsigned int)(distancePattern) << 4) | (unsigned int)shooterPattern);
+	m_i2c->Write(84,combinedPattern);
+}
+
+void Indicator::UpdateDisabled()
+{
+	char shooterPattern = (char)IndicatorData::SHOOTER_OFF;
+	char distancePattern = (char)IndicatorData::DISTANCE_OFF;
+	char combinedPattern = (char)(((unsigned int)(distancePattern) << 4) | (unsigned int)shooterPattern);
+	m_i2c->Write(84,combinedPattern);
+}
+
