6b827206c87abecdf3bc054fb44e7f61190787f8
tonypeng1337 <tonypeng1337@f74aeb85-c29b-4e46-8eb1-f4d822aa94e9>
2014-03-15 03:06:14 +0000

    add autonomous functionality to fakehotgoal

--- Utilities/FakeHotGoal/FakeHotGoal/HotGoalWindow.cs
+++ Utilities/FakeHotGoal/FakeHotGoal/HotGoalWindow.cs
@@ -17,41 +17,91 @@ namespace FakeHotGoal
     public partial class HotGoalWindow : Form
     {
         private const byte HOT_GOAL_HEADER = 0x00;
+        private const int FIVE_SECONDS = 5000;
 
         HotGoal _hotGoal;
         NetClient _client;
+
+        AutonomousCountdown countdown;
+
+        volatile bool _isAuton;
+        volatile int _autonStart;
 
         Thread _updateThread;
 
         public HotGoalWindow()
         {
             _hotGoal = HotGoal.NONE;
+            _isAuton = false;
+
+            countdown = new AutonomousCountdown();
+
+            _updateThread = new Thread(UpdateTask);
+            _updateThread.IsBackground = false;
+            _updateThread.Start();
 
             InitializeComponent();
         }
 
         public void UpdateTask()
         {
-            bool lastVal = _client.Connected;
+            bool lastVal = _client != null && _client.Connected;
+            bool autonLastWasMoreThanFiveSeconds = false;
 
             while (true)
             {
-                if (_client.Connected && !lastVal)
+                if (_client != null)
                 {
-                    statusStrip1.Invoke((MethodInvoker)(() => statusLabel.Text = "Connected."));
-                    menuStrip1.Invoke((MethodInvoker)(() => connectToolStripMenuItem.Enabled = false));
-                    menuStrip1.Invoke((MethodInvoker)(() => disconnectToolStripMenuItem.Enabled = true));
-                }
-                else if (!_client.Connected && lastVal)
-                {
-                    statusStrip1.Invoke((MethodInvoker)(() => statusLabel.Text = "Disconnected."));
-                    menuStrip1.Invoke((MethodInvoker)(() => connectToolStripMenuItem.Enabled = true));
-                    menuStrip1.Invoke((MethodInvoker)(() => disconnectToolStripMenuItem.Enabled = false));
+                    if (_client.Connected && !lastVal)
+                    {
+                        statusStrip1.Invoke((MethodInvoker)(() => statusLabel.Text = "Connected."));
+                        menuStrip1.Invoke((MethodInvoker)(() => connectToolStripMenuItem.Enabled = false));
+                        menuStrip1.Invoke((MethodInvoker)(() => disconnectToolStripMenuItem.Enabled = true));
+                    }
+                    else if (!_client.Connected && lastVal)
+                    {
+                        statusStrip1.Invoke((MethodInvoker)(() => statusLabel.Text = "Disconnected."));
+                        menuStrip1.Invoke((MethodInvoker)(() => connectToolStripMenuItem.Enabled = true));
+                        menuStrip1.Invoke((MethodInvoker)(() => disconnectToolStripMenuItem.Enabled = false));
+                    }
+
+                    lastVal = _client.Connected;
                 }
 
-                lastVal = _client.Connected;
+                if (_isAuton)
+                {
+                    int end = _autonStart + 2 * FIVE_SECONDS;
 
-                Thread.Sleep(1000);
+                    int millisLeft = end - Environment.TickCount;
+                    if (millisLeft < 0) millisLeft = 0;
+
+                    int secondsLeft = (millisLeft + 500) / 1000;
+
+                    if (secondsLeft < 5 && !autonLastWasMoreThanFiveSeconds)
+                    {
+                        if (_hotGoal == HotGoal.LEFT) _hotGoal = HotGoal.RIGHT;
+                        else if (_hotGoal == HotGoal.RIGHT) _hotGoal = HotGoal.LEFT;
+
+                        SendState();
+
+                        updateButtons();
+                    }
+
+                    if (secondsLeft <= 0)
+                    {
+                        _isAuton = false;
+                        countdown.Invoke((MethodInvoker)(() => countdown.Hide()));
+                        menuStrip1.Invoke((MethodInvoker)(() => autonomousToolStripMenuItem.Enabled = true ));
+                    }
+
+                    autonLastWasMoreThanFiveSeconds = secondsLeft < 5;
+
+                    countdown.Invoke((MethodInvoker)(() => countdown.SetCounter(secondsLeft)));
+                }
+                else
+                    autonLastWasMoreThanFiveSeconds = false;
+
+                Thread.Sleep(100);
             }
         }
 
@@ -62,7 +112,7 @@ namespace FakeHotGoal
 
         private void SendState()
         {
-            if (!_client.Connected) return;
+            if (_client == null || !_client.Connected) return;
 
             NetBuffer nb = new NetBuffer(2);
 
@@ -72,44 +122,58 @@ namespace FakeHotGoal
             _client.Send(nb, NetChannel.NET_RELIABLE_SEQUENCED, 1);
         }
 
+        private void updateButtons()
+        {
+            hotgoalLeft.FlatAppearance.BorderColor = Color.Black;
+            hotgoalRight.FlatAppearance.BorderColor = Color.Black;
+
+            if (_hotGoal == HotGoal.LEFT)
+                hotgoalLeft.FlatAppearance.BorderColor = Color.Yellow;
+
+            if (_hotGoal == HotGoal.RIGHT)
+                hotgoalRight.FlatAppearance.BorderColor = Color.Yellow;
+        }
+
         private void hotgoalLeft_Click(object sender, EventArgs e)
         {
+            if (_isAuton) return;
+
             if (_hotGoal == HotGoal.LEFT)
             {
-                hotgoalLeft.FlatAppearance.BorderColor = Color.Black;
                 _hotGoal = HotGoal.NONE;
 
                 SendState();
+
+                updateButtons();
                 
                 return;
             }
 
-            if (_hotGoal == HotGoal.RIGHT) hotgoalRight.FlatAppearance.BorderColor = Color.Black;
-
-            hotgoalLeft.FlatAppearance.BorderColor = Color.Yellow;
-
             _hotGoal = HotGoal.LEFT;
 
             SendState();
+
+            updateButtons();
         }
 
         private void hotgoalRight_Click(object sender, EventArgs e)
         {
+            if (_isAuton) return;
+
             if (_hotGoal == HotGoal.RIGHT)
             {
-                hotgoalRight.FlatAppearance.BorderColor = Color.Black;
                 _hotGoal = HotGoal.NONE;
 
                 SendState();
 
+                updateButtons();
+
                 return;
             }
 
-            if (_hotGoal == HotGoal.LEFT) hotgoalLeft.FlatAppearance.BorderColor = Color.Black;
-
-            hotgoalRight.FlatAppearance.BorderColor = Color.Yellow;
-
             _hotGoal = HotGoal.RIGHT;
+
+            updateButtons();
 
             SendState();
         }
@@ -120,12 +184,8 @@ namespace FakeHotGoal
 
             _client = new NetClient();
 
-            _updateThread = new Thread(UpdateTask);
-            _updateThread.IsBackground = false;
-            _updateThread.Start();
-
             _client.Open();
-            _client.Connect(new IPEndPoint(IPAddress.Parse("10.8.46.2"), 846));
+            _client.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 846));
 
             statusLabel.Text = "Connecting...";
         }
@@ -143,6 +203,26 @@ namespace FakeHotGoal
 
             disconnectToolStripMenuItem.Enabled = false;
             connectToolStripMenuItem.Enabled = true;
+        }
+        
+        private void autonomousToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (_isAuton) return;
+
+            autonomousToolStripMenuItem.Enabled = false;
+
+            _autonStart = Environment.TickCount;
+            _isAuton = true;
+
+            Random rand = new Random();
+            _hotGoal = (HotGoal)rand.Next(0, 2);
+
+            SendState();
+
+            updateButtons();
+            
+            countdown.SetCounter(10);
+            countdown.Show();
         }
     }
 }
