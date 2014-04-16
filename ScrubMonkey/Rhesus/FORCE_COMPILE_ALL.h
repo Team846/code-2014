e0d32000b45e2cad76b36413191b341b4d7f9805
tonypeng1337 <tonypeng1337@f74aeb85-c29b-4e46-8eb1-f4d822aa94e9>
2014-04-16 01:13:44 +0000

    Restore some overwritten props...

---
+++ ScrubMonkey/Rhesus/FORCE_COMPILE_ALL.h
@@ -0,0 +1,20 @@
+// WARNING: THIS FILE SHOULD ONLY BE INCLUDED TO TEST COMPILATION OF ALL FILES
+// DO NOT INCLUDE ON PRODUCTION/DEVELOPMENT BUILDS OR YOUR COMPILE TIME WILL CRAWL INTO A HOLE AND DIE.
+
+#ifndef FORCE_COMPILE_ALL_H_
+#define FORCE_COMPILE_ALL_H_
+
+#include <WPILib.h>
+
+#include "Toolkit/Rhesus.Toolkit.h"
+#include "Toolkit/Action/Rhesus.Toolkit.Action.h"
+#include "Toolkit/Configuration/Rhesus.Toolkit.Configuration.h"
+#include "Toolkit/Control/Rhesus.Toolkit.Control.h"
+#include "Toolkit/Diagnostics/Rhesus.Toolkit.Diagnostics.h"
+#include "Toolkit/InterCommunication/Rhesus.Toolkit.InterCommunication.h"
+#include "Toolkit/IO/Rhesus.Toolkit.IO.h"
+#include "Toolkit/Scripting/Rhesus.Toolkit.Scripting.h"
+#include "Toolkit/Tasks/Rhesus.Toolkit.Tasks.h"
+#include "Toolkit/Utilities/Rhesus.Toolkit.Utilities.h"
+
+#endif
