e392bcfed42501b8b8dcca81802122e51f6dd0b3
manojvasishta <manojvasishta@f74aeb85-c29b-4e46-8eb1-f4d822aa94e9>
2014-04-18 03:14:17 +0000

    face hotgoal works for right, center, and left alignment needs testing

--- LRT14/Brain/Automation/FaceHotGoal.h
+++ LRT14/Brain/Automation/FaceHotGoal.h
@@ -2,42 +2,20 @@
 #define FACE_HOT_GOAL_H_
 
 #include "Automation.h"
+#include "Turn.h"
+#include "../../Sensors/CheesyVisionServer.h"
 
-#include "JitterTurn.h"
-
-#include "../../Config/Configurable.h"
-
-#include "../../Sensors/HotGoal.h"
-#include "../../Sensors/SensorFactory.h"
-
-class FaceHotGoal : public JitterTurn, public Configurable
+class FaceHotGoal : public Turn
 {
 public:
-	FaceHotGoal();
-	FaceHotGoal(bool clear, bool turnToCenter);
-	FaceHotGoal(bool clear, bool turnToCenter, double randLow, double randHigh);
-	
-	void Configure();
+	FaceHotGoal(std::string startSide, double turnDegrees);
 	
 	bool Start();
 	
-	static void Reset()
-	{
-		m_lastHotGoalSide = HotGoal::NONE_ACTIVE;
-	}
-	
-	static HotGoal::Side LastHotGoalSide()
-	{
-		return m_lastHotGoalSide;
-	}
-	
 private:
-	static HotGoal::Side m_lastHotGoalSide;
-	static int m_hotGoalPos[3];
-	
-	static double s_turnDegrees;
-
-	bool m_turnToCenter;
+	CheesyVisionServer* m_hotGoal;
+	double m_turnDegrees;
+	std::string m_startSide;
 };
 
 #endif
